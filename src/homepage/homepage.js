7eea583973bc28d27629868c4a5dca25b6f547db
Ibikunle Jamiu <ibidotun@gmail.com>
2022-06-20 19:25:09 +0100

    Made changes to the mockup

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -6,21 +6,26 @@ import customerservice from '../resources/customerservice.png'
 import mockup from '../resources/mockup.png'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { Forward } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
 
 	AOS.init()
 
 	return (
-		<main className={styles.container}>
-			<section className={styles.header}>
+		<main className={styles.homecontainer}>
+			<section className={styles.mockupheader}>
 				<aside className={styles.welcome} data-aos='fade-right'>
 					<p className={styles.welcomebig}>
-						No. 1 Marketplace for Mobile Gadgets.
+						No. 1 Online Marketplace for Mobile Gadgets.
 					</p>
 					<p className={styles.welcomesmall}>
 						Buy from the comfort of your phone and enjoy the best deals.
 					</p>
+				<Link to='/shopping' className={styles.goshopping}>
+					<Forward fontSize='small' /> GO SHOPPING
+				</Link>
 				</aside>
 				<aside className={styles.mockup1wrapper}>
 					<img src={mockup} alt='Mockup' className={styles.mockup1} data-aos='fade-left' data-aos-delay='300' />
@@ -46,8 +51,8 @@ const Homepage = () => {
 				<img src={delivery} alt='mockup2' className={styles.mockup2} data-aos='fade-left' data-aos-delay='600' />
 			</section>
 			<section className={styles.customerservice}>
-				<img src={customerservice} alt='mockup2' className={styles.mockup2} data-aos='fade-right' />
-				<aside className={styles.whyusanswers} data-aos='fade-left' data-aos-delay='300' >
+				<img src={customerservice} alt='mockup2' className={styles.mockup2} data-aos='fade-right' data-aos-delay='900' />
+				<aside className={styles.whyusanswers} data-aos='fade-left' data-aos-delay='1200' >
 					<p className={styles.answer}>
 					We know customer service is instrumental to the success of a business. That is why we take great pride in our stellar customer service.
 					</p>
