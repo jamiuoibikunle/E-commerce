8b9c535a5535feffb932c39c0311bb6b8a8bec87
Ibikunle Jamiu <ibidotun@gmail.com>
2022-06-20 20:32:33 +0100

    Made changes to the answers and questions

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -31,32 +31,34 @@ const Homepage = () => {
 					<img src={mockup} alt='Mockup' className={styles.mockup1} data-aos='fade-left' data-aos-delay='300' />
 				</aside>
 			</section>
+			<section className={styles.whyus}>
 					<p className={styles.question} data-aos='fade-up'>
 						Why Buy From Us?
 					</p>
 			<section className={styles.reliability}>
-				<img src={handshake} alt='mockup2' className={styles.mockup2} data-aos='fade-right' data-aos-delay='300' />
-				<aside className={styles.whyusanswers} data-aos='fade-left' data-aos-delay='600'>
+				<img src={handshake} alt='mockup2' className={styles.mockup2} data-aos='fade-right' />
+				<aside className={styles.whyusanswers} data-aos='fade-up' data-aos-delay='300'>
 					<p className={styles.answer}>
 						We are all about reliability, we want you to be able to trust our products, trust that they will last and withstand all kind of environments.
 					</p>
 				</aside>
 			</section>
 			<section className={styles.delivery}>
-				<aside className={styles.whyusanswers} data-aos='fade-right' data-aos-delay='900'>
+				<aside className={styles.whyusanswers} data-aos='fade-up' data-aos-delay='300'>
 					<p className={styles.answer}>
 						We deliver nationwide across Nigeria every day of the week. It is our obligation to ensure our customers do not wait longer than need be.
 					</p>
 				</aside>
-				<img src={delivery} alt='mockup2' className={styles.mockup2} data-aos='fade-left' data-aos-delay='600' />
+				<img src={delivery} alt='mockup2' className={styles.mockup2} data-aos='fade-left' />
 			</section>
 			<section className={styles.customerservice}>
-				<img src={customerservice} alt='mockup2' className={styles.mockup2} data-aos='fade-right' data-aos-delay='900' />
-				<aside className={styles.whyusanswers} data-aos='fade-left' data-aos-delay='1200' >
+				<img src={customerservice} alt='mockup2' className={styles.mockup2} data-aos='fade-right' />
+				<aside className={styles.whyusanswers} data-aos='fade-up' data-aos-delay='300' >
 					<p className={styles.answer}>
 					We know customer service is instrumental to the success of a business. That is why we take great pride in our stellar customer service.
 					</p>
 				</aside>
+			</section>
 			</section>
 		</main>		
 	)
